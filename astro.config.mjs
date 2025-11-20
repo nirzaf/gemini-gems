dd4188c75aacbb8d29acc1470d2676c605c92b15
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2025-11-20 07:48:36 +0000

    feat: Integrate Tailwind CSS using `@astrojs/tailwind` and configure its settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dotnetevangelist.net',
   base: '/gemini-gems',
   output: 'static',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind()]
 });
