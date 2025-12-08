7f1e4066b7f2eef289811b3806f4b48bb4924132
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2025-12-08 10:07:46 +0000

    feat: Add Netlify adapter for server-side Astro deployment and update gitignore.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 ﻿// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://geminigems.netlify.app',
-  output: 'static',
+  output: 'server', // Server mode to enable API routes
+  adapter: netlify(),
   integrations: [tailwind()]
 });
